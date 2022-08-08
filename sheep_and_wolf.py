212611cae0da8877fae23cf7d9e363a0497e5b92
chaeminsoo <chaems97@gmail.com>
2022-08-08 17:54:43 +0900

    DFS

--- sheep_and_wolf.py
+++ sheep_and_wolf.py
@@ -1,24 +1,27 @@
+answer = 0
 def dfs(now,s,w,visited,info,edge):
     global answer
-    if s != 0 and s <= w:
-        return
-    
-    if info[now] == 0: s+=1
-    elif info[now] == 1: w+=1
+    if info[now] == 0:
+        s+=1
+    elif info[now] == 1:
+        w+=1
     else: pass
-    info[now] = -1
-    print('==',s,'/',w,'/',info[now],now)
-    visited[now][s][w] = True
+
+    if s <= w: return
+    else: answer = max(answer,s)
+    
     for i in edge[now]:
-        if visited[i][s][w] == False:
+        if not visited[i][s][w]:
+            ref = info[now]
+            info[now] = -1
+            visited[now][s][w] = True
             dfs(i,s,w,visited,info,edge)
-    answer = max(answer,s)
-    print('++',answer)
+            visited[now][s][w] = False
+            info[now] = ref
     return
 
 def solution(info, edges):
     global answer
-    answer = 0
     s_num = 0
     w_num = 0
     for i in info:
@@ -29,9 +32,5 @@ def solution(info, edges):
         edge[i].append(j)
         edge[j].append(i)
     visited = [[[False]*(w_num+1) for i in range(s_num+1)] for j in range(s_num+w_num)]
-    # for i in visited:
-    #     print(i)
     dfs(0,0,0,visited,info,edge)
     return answer
-
-print(solution([0,0,1,1,1,0,1,0,1,0,1,1],[[0,1],[1,2],[1,4],[0,8],[8,7],[9,10],[9,11],[4,3],[6,5],[4,6],[8,9]]))
