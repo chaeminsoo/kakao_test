94a248815dd31a294e6f38de43d29e7490688e67
chaeminsoo <chaems97@gmail.com>
2022-08-15 23:54:44 +0900

    Incomplete

--- cards_pairs.py
+++ cards_pairs.py
@@ -1,6 +1,42 @@
 # 2021 KAKAO BLIND RECRUITMENT
 from itertools import permutations
+from collections import deque
 
+def find_card(n):
+    ref = []
+    for i in range(4):
+        for j in range(4):
+            if board[i][j] == n:
+                ref.append([i,j])
+    return ref
+
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+def remove_card(board,cloc,aloc,bloc):
+    ref_b = [i[:] for i in board]
+    ans = 1e9
+    x,y = cloc
+    for st, ed in [[aloc, bloc], [bloc, aloc]]:
+        
+        for i, j in [[cloc, st], [st, ed]]:
+            r,c = i
+            q = deque()
+            q.append([r,c,0])
+            visited = [[False]*4 for _ in range(4)]
+            range_board = [[0]*4 for _ in range(4)]
+            while q:
+                x,y,v = q.popleft()
+                range_board[x][y] = v
+                
+                for k in range(4):
+                    nx = x + dx[k]
+                    ny = y + dy[k]
+                    
+                    if 0 <= nx < 4 and 0 <= ny < 4 and not visited[nx][ny]:
+                        if ref_b[nx][ny] != 0 and ref_b[x][y] == 0:
+                            q.append([nx,ny,1 if v == 0 else v])
+                            
 def solution(board, r, c):
     answer = 0
     cards = []
@@ -16,12 +52,3 @@ def solution(board, r, c):
             
             
     return answer
-
-
-
-
-
-
-
-
-
